8905ba6e14caaa96ddc7f61084c3d33ec8e26be6
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-10-11 09:02:48 +0200

    Add a sample component with a spec file
    Show cases enzyme tests, stubbing and module injection

--- src/components/HelloComponent.js
+++ src/components/HelloComponent.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import * as Title from '@klarna/ui/Title'
+import exampleDep from './exampleDep'
+import styles from './HelloComponent.css'
 
 export default function HelloComponent () {
+  const dynamicText = `This is from a dependency: ${exampleDep()}`
   return (
-    <h1>Hello world</h1>
+    <div className={styles.helloComponent}>
+      <Title.Primary>Hello world</Title.Primary>
+      <Title.Secondary>
+        {dynamicText}
+      </Title.Secondary>
+    </div>
   )
 }
 
--- src/components/HelloComponent.spec.js
+++ src/components/HelloComponent.spec.js
@@ -1,10 +1,32 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import HelloComponent from './HelloComponent'
+import { mount } from 'enzyme'
+import * as Title from '@klarna/ui/Title'
+import HelloComponentInjector from 'inject!./HelloComponent'
+
+const exampleDepStub = sinon.stub().returns('fixed output')
+
+const HelloComponent = HelloComponentInjector({
+  './exampleDep': exampleDepStub
+}).default
 
 describe('HelloComponent', () => {
-  it('should contain "Hello world"', () => {
-    const wrapper = shallow(<HelloComponent />)
-    expect(wrapper.text()).toEqual('Hello world')
+  it('should contain a "Hello world" title', () => {
+    const wrapper = mount(<HelloComponent />)
+    const title = wrapper.find(Title.Primary)
+
+    const expected = 'Hello world'
+    const actual = title.text()
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should contain a secondary title with text from exampleDep', () => {
+    const wrapper = mount(<HelloComponent />)
+    const title = wrapper.find(Title.Secondary)
+
+    const expected = 'This is from a dependency: fixed output'
+    const actual = title.text()
+
+    expect(actual).toEqual(expected)
   })
 })
