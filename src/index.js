0719a5c277222f3078a8f99b91959c24a8e786e0
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-10-11 09:01:15 +0200

    Render the root of the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import HelloComponent from './components/HelloComponent'
+import './index.css'
+
+const RootComponent = React.createClass({
+  render: () => (
+    <HelloComponent />
+  )
+})
 
 const rootElement = document.getElementById('tdd-workshop')
-ReactDOM.render(<HelloComponent />, rootElement)
+ReactDOM.render(<RootComponent />, rootElement)
 
